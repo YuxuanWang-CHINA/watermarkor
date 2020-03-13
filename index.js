efaa0056d1be14ab5a723da5661817a7d7fd884c
Yuxuan_Wang <36295581+YuxuanWang-CHINA@users.noreply.github.com>
2020-03-13 12:49:29 +0800

    v3

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const jimp = require('jimp');
 
 var watermarkor = function (input1, input2) {
-    //input
-    const imageoLoaction = input1
-    const watermarkLocation = input2
-    var imageo={};
-    var watermark={};
-    jimp.read(imageoLoaction).then(ima => imageo=ima)
-        .then(() => {
-            jimp.read(watermarkLocation).then(ima => watermark = ima)
+    return new Promise((resolve,reject)=>{
+        const imageoLoaction = input1
+        const watermarkLocation = input2
+        var imageo={};
+        var watermark={};
+        jimp.read(imageoLoaction).then(ima => imageo=ima)
             .then(() => {
-                imageo.composite(watermark, imageo.bitmap.width - watermark.bitmap.width, imageo.bitmap.height - watermark.bitmap.height)
-                    .getBuffer(jimp.AUTO,(e,buf)=>{return buf})
+                jimp.read(watermarkLocation).then(ima => watermark = ima)
+                .then(() => {
+                    imageo.composite(watermark, imageo.bitmap.width - watermark.bitmap.width, imageo.bitmap.height - watermark.bitmap.height)
+                        .getBuffer(jimp.AUTO,(e,buf)=>{resolve (buf)})
+                })
+                    .catch((e) => { console.log(e); reject(e) })
             })
-            .catch((e) => { console.log(e); return e })
-        })
-        .catch((e) => { console.log(e); return e })
+            .catch((e) => { console.log(e); reject (e) })
+    })
 }
-
-//watermarkor('./resources/input.jpg', './resources/watermark.png')
 
 module.exports = {
     watermarkor: watermarkor
