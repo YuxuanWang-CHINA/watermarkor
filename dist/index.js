bb906967d6b5ef529060c77912e55e7d78ef5159
Yuxuan_Wang <36295581+YuxuanWang-CHINA@users.noreply.github.com>
2020-03-20 19:17:10 +0800

    big update

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,50 @@
 "use strict";
 var jimp = require("jimp");
-var watermarkor = function (oimageLocation, watermarkLocation) {
+var readAll = function (imageLocationArray) {
     return new Promise(function (resolve, reject) {
-        Promise.all([jimp.read(oimageLocation), jimp.read(watermarkLocation)]).then(function (imgobj) {
-            imgobj[0]
-                .composite(imgobj[1], imgobj[0].bitmap.width - imgobj[1].bitmap.width, imgobj[0].bitmap.height - imgobj[1].bitmap.height)
-                .getBufferAsync(imgobj[0].getMIME())
-                .then(function (tarbuf) {
-                return resolve(tarbuf);
+        var re = [];
+        for (var _i = 0, imageLocationArray_1 = imageLocationArray; _i < imageLocationArray_1.length; _i++) {
+            var v = imageLocationArray_1[_i];
+            re.push(jimp.read(v));
+        }
+        Promise.all(re).then(function (res) {
+            return resolve(res);
+        });
+    });
+};
+var addOne = function (imageJimp, watermarkJimp) {
+    return new Promise(function (resolve, reject) {
+        imageJimp
+            .composite(watermarkJimp, imageJimp.bitmap.width - watermarkJimp.bitmap.width, imageJimp.bitmap.height - watermarkJimp.bitmap.height)
+            .getBufferAsync(imageJimp.getMIME())
+            .then(function (tarbuf) {
+            return resolve(tarbuf);
+        });
+    });
+};
+var addAll = function (imageJimpArray, watermarkJimp) {
+    return new Promise(function (resolve, reject) {
+        var rePromise = [];
+        for (var _i = 0, imageJimpArray_1 = imageJimpArray; _i < imageJimpArray_1.length; _i++) {
+            var oimage = imageJimpArray_1[_i];
+            rePromise.push(addOne(oimage, watermarkJimp));
+        }
+        Promise.all(rePromise).then(function (res) {
+            return resolve(res);
+        });
+    });
+};
+var watermarkor = function (imagesLocationArray, watermarkLocation) {
+    return new Promise(function (resolve, reject) {
+        Promise.all([readAll(imagesLocationArray), jimp.read(watermarkLocation)]).then(function (_a) {
+            var imageJimpArray = _a[0], watermarkJimp = _a[1];
+            addAll(imageJimpArray, watermarkJimp).then(function (res) {
+                console.log(res);
+                return resolve(res);
             });
         });
     });
 };
+watermarkor(['./resources/input1.jpg', './resources/input2.jpg'], './resources/watermark.png');
 module.exports = watermarkor;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,57 @@
 import * as jimp from 'jimp';
 
-let watermarkor = function(oimageLocation: string, watermarkLocation: string) {
+let readAll = function(imageLocationArray: string[]) {
+    return new Promise<jimp[]>((resolve, reject) => {
+        let re: object[] = [];
+        for (let v of imageLocationArray) {
+            re.push(jimp.read(v));
+        }
+        Promise.all(re).then((res: jimp[]) => {
+            return resolve(res);
+        });
+    });
+};
+
+let addOne = function(imageJimp, watermarkJimp) {
     return new Promise<Buffer>((resolve, reject) => {
-        Promise.all([jimp.read(oimageLocation), jimp.read(watermarkLocation)]).then(
-            (imgobj: jimp[]) => {
-                imgobj[0]
-                    .composite(
-                        imgobj[1],
-                        imgobj[0].bitmap.width - imgobj[1].bitmap.width,
-                        imgobj[0].bitmap.height - imgobj[1].bitmap.height
-                    )
-                    .getBufferAsync(imgobj[0].getMIME())
-                    .then((tarbuf) => {
-                        return resolve(tarbuf);
-                    });
+        imageJimp
+            .composite(
+                watermarkJimp,
+                imageJimp.bitmap.width - watermarkJimp.bitmap.width,
+                imageJimp.bitmap.height - watermarkJimp.bitmap.height
+            )
+            .getBufferAsync(imageJimp.getMIME())
+            .then((tarbuf: Buffer) => {
+                return resolve(tarbuf);
+            });
+    });
+};
+
+let addAll = function(imageJimpArray: jimp[], watermarkJimp: jimp) {
+    return new Promise<Buffer[]>((resolve, reject) => {
+        let rePromise: object[] = [];
+        for (let oimage of imageJimpArray) {
+            rePromise.push(addOne(oimage, watermarkJimp));
+        }
+        Promise.all(rePromise).then((res: Buffer[]) => {
+            return resolve(res);
+        });
+    });
+};
+
+let watermarkor = function(imagesLocationArray: string[], watermarkLocation: string) {
+    return new Promise<Buffer[]>((resolve, reject) => {
+        Promise.all([readAll(imagesLocationArray), jimp.read(watermarkLocation)]).then(
+            ([imageJimpArray, watermarkJimp]) => {
+                addAll(imageJimpArray, watermarkJimp).then((res) => {
+                    console.log(res);
+                    return resolve(res);
+                });
             }
         );
     });
 };
 
 export = watermarkor;
+
+watermarkor(['./resources/input1.jpg', './resources/input2.jpg'], './resources/watermark.png');
